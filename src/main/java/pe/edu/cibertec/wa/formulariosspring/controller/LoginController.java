3e0bd39ecd7c7624916ddcf246b911185352207e
Gabriel <gmachicaoq@unsa.edu.pe>
2024-03-31 11:11:50 -0500

    Subida del proyecto

---
+++ src/main/java/pe/edu/cibertec/wa/formulariosspring/controller/LoginController.java
@@ -0,0 +1,12 @@
+package pe.edu.cibertec.wa.formulariosspring.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
---
+++ src/main/java/pe/edu/cibertec/wa/formulariosspring/model/LoginModel.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.wa.formulariosspring.model;
+
+public class LoginModel {
+}
